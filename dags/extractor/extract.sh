0a32c935a0c7c621b75247f700bc3b3fc1378bb4
Ubuntu <ubuntu@ip-172-31-41-122.sa-east-1.compute.internal>
2022-10-25 15:49:53 +0000

    Using credentials file to config gcloud

--- dags/extractor/extract.sh
+++ dags/extractor/extract.sh
@@ -16,15 +16,18 @@ if [[ $local = "dev" ]]; then # Checking if manual GCS credential is needed
     echo "LOCAL DEVELOPMENT ENV!!"
     if ! [[ $(ls credentials/ | grep gcs_credential.json) ]]; then
         echo "Creating a local GCS Credential File into credentials/gcs_credential.json"
-        terraform output service_credential_private >> credentials/gcs_credential.json
-        gsutil -o Credentials:gs_service_key_file=${PWD}/credentials/gcs_credential.json cp temp_download gs://${bucket}/landing_zone/${file}
+        terraform -chdir=$PWD/../../terraform/dl_gcs/ output service_credential_private >> credentials/gcs_credential.json
     else
         echo "GCS Credential File already exists."
     fi
 else
     echo "NOT LOCAL DEVELOPMENT ENV"
-    gsutil cp temp_download gs://${bucket}/landing_zone/${file}
 fi
+
+echo "Activating gcloud with credentials"
+gcloud auth activate-service-account --key-file=credentials/gcs_credential.json
+
+gsutil cp temp_download gs://${bucket}/landing_zone/${file}
 
 echo "Data Lake Bucket to insert data ${bucket}"
 echo "File to be transfered from FTP server to DL ftp://ftp.mtps.gov.br/%2f${file}"
